9bceb53971f91c357c9dd8159382adbc99f69e7d
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2022-11-22 15:53:29 +0100

    Read reactive-gui01 and started reactive-gui02 trying ExecutorService

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,8 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,4 +15,37 @@ import javax.swing.SwingUtilities;
  * Second example of reactive GUI.
  */
 public final class ConcurrentGUI extends JFrame {
+
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+
+    public ConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        ExecutorService es = Executors.newSingleThreadExecutor((c) -> new Thread(new Counter()));
+    }
+
+    private class Counter implements Runnable {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            
+        }
+
+    }
+
 }
