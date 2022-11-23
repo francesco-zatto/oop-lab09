bb3e2b4a96f4e9b52eeb4dcdfd074f3415d940ef
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2022-11-23 19:59:42 +0100

    In 92-workers completed every test and added javadoc where necessary.

--- 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -3,44 +3,37 @@ package it.unibo.oop.workers02;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MultiThreadedSumMatrix implements SumMatrix {
+final public class MultiThreadedSumMatrix implements SumMatrix {
 
     private final int numWorkers;
 
+    /**
+     * @param numWorkers. Initialize an instance with the number of threads.
+     */
     public MultiThreadedSumMatrix(final int numWorkers) {
         this.numWorkers = numWorkers;
     }
 
     private class Worker extends Thread {
-        private final List<Double> matrixToList;
-        private final int numWorkers;
+        private final List<double[]> rowsList;
         private final int startPos;
         private final int stopPos;
         private double partialSum;
 
-        public Worker(final double[][] matrix, final int numWorkers, final int startPos, final int stopPos) {
-            this.matrixToList = toList(matrix);
-            this.numWorkers = numWorkers;
+        Worker(final List<double[]> matrixToList, final int startPos, final int stopPos) {
             this.startPos = startPos;
             this.stopPos = stopPos;
+            this.rowsList = matrixToList.subList(startPos, stopPos);
         }
 
         @Override
         public void run() {
             System.out.println("Starting from " + this.startPos + " to " + this.stopPos);
-            for (int i = this.startPos; i < this.stopPos; i++) {
-                partialSum = partialSum + this.matrixToList.get(i);
-            }
-        }
-
-        private List<Double> toList(double[][] matrix) {
-            final List<Double> list = new ArrayList<>();
-            for (final double[] row : matrix) {
-                for (final double elem : row) {
-                    list.add(elem);
+            for (var row : this.rowsList) {
+                for (double elem : row) {
+                    this.partialSum = partialSum + elem;
                 }
             }
-            return list;
         }
 
         public double getPartialSum() {
@@ -49,26 +42,43 @@ public class MultiThreadedSumMatrix implements SumMatrix {
 
     }
 
+    /* 
+     * A method that takes a matrix and sums all its elements using more than
+     * one thread.
+     */
     @Override
-    public double sum(double[][] matrix) {
-        Double sum = 0.0;
-        final int nelem = matrix.length * numMatrixCols(matrix) / this.numWorkers;
-        //TODO: calcola il resto
-        int startPos = 0;
+    public double sum(final double[][] matrix) {
+        final List<double[]> rowsList = toRowsList(matrix);
         final List<Worker> workers = new ArrayList<>();
-        for (int i = 1; i <= this.numWorkers; i++) {
-            workers.add(new Worker(matrix, this.numWorkers, startPos, startPos + nelem));
+        final int lastWorker = this.numWorkers - 2;
+        int rowsPerThreads = rowsList.size() / this.numWorkers;
+        final int divRest = rowsList.size() % this.numWorkers;
+        Double sum = 0.0;
+        int startRow = 0;
+        for (int i = 0; i < this.numWorkers; i++) {
+            workers.add(new Worker(rowsList, startRow, startRow + rowsPerThreads));
             workers.get(i).start();
-            startPos = i == this.numWorkers ? //TODO ;            
+            startRow = startRow + rowsPerThreads;
+            if (i == lastWorker) {
+                rowsPerThreads = rowsPerThreads + divRest;
+            }
+        }
+        for (var worker : workers) {
+            try {
+                worker.join();
+                sum = sum + worker.getPartialSum();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return sum;
     }
 
-    private int numMatrixCols(double[][] matrix) {
-        for (double[] row : matrix) {
-            return row.length;
+    private static List<double[]> toRowsList(final double[][] matrix) {
+        final List<double[]> list = new ArrayList<>();
+        for (final double[] row : matrix) {
+            list.add(row);
         }
-        return 0;
+        return list;
     }
-    
 }
